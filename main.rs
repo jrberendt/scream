8a5e962901aec3ccf29ae546b57b0ca9d676f5ff
Jack Berendt <43920418+jrberendt@users.noreply.github.com>
2022-10-18 20:47:11 -0400

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,31 @@
+fn main() {
+    let mut commands = Vec::new();
+    println!("Generating the 4-bit adding machine.");
+    for number1 in 1..15{
+        for number2 in 1..15{ 
+            commands.push(format!("{}+{}", number1, number2));
+            commands.push(format!("{}", number1 + number2));
+        }
+    } 
+    for number1 in 1..15{
+        for number2 in 1..15{ 
+            commands.push(format!("{}-{}", number1, number2));
+            commands.push(format!("{}", number1 - number2));
+        }
+    }
+
+    loop{
+        let mut command_input = String::new();
+        std::io::stdin().read_line(&mut command_input).expect("Could not read from stdio.");
+        let mut print = false;
+        for command in commands.clone(){
+            if print{
+                println!("{command}");
+                break;
+            }
+            if command.trim() == command_input.trim(){
+                print = true;
+            }
+        }
+    }
+}
